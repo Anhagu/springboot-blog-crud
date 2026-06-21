36de6220e8799ff15e061580cc019d95c54eda0d
Anhagu <leejhsam330@gmail.com>
2026-06-22 02:25:37 +0900

    블로그 화면 구성

---
+++ src/main/java/me/leejaehyeong/springbootblog/dto/ArticleViewResponse.java
@@ -0,0 +1,23 @@
+package me.leejaehyeong.springbootblog.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.leejaehyeong.springbootblog.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
