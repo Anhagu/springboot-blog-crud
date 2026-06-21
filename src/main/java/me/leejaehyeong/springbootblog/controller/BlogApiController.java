d1f78fc7fe5f8e740b119bb83f6a6d66aa3f8702
Anhagu <leejhsam330@gmail.com>
2026-06-22 01:41:09 +0900

    CRUD 구현

--- src/main/java/me/leejaehyeong/springbootblog/controller/BlogApiController.java
+++ src/main/java/me/leejaehyeong/springbootblog/controller/BlogApiController.java
@@ -3,10 +3,13 @@ package me.leejaehyeong.springbootblog.controller;
 import lombok.RequiredArgsConstructor;
 import me.leejaehyeong.springbootblog.domain.Article;
 import me.leejaehyeong.springbootblog.dto.AddArticleRequest;
+import me.leejaehyeong.springbootblog.dto.ArticleResponse;
 import me.leejaehyeong.springbootblog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -18,5 +21,31 @@ public class BlogApiController {
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article saveArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable Long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/me/leejaehyeong/springbootblog/service/BlogService.java
+++ src/main/java/me/leejaehyeong/springbootblog/service/BlogService.java
@@ -7,6 +7,8 @@ import me.leejaehyeong.springbootblog.dto.AddArticleRequest;
 import me.leejaehyeong.springbootblog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,18 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(Long id) {
+        blogRepository.deleteById(id);
     }
 }
